7b07d84e5fc58757a426bb7290aba08414cd4148
canxin <q1969730106@gmail.com>
2025-02-14 11:46:33 +0800

    [Chnage] change env var name

--- http_proxy_service.sh
+++ http_proxy_service.sh
@@ -3,6 +3,6 @@
 
 MAGISK_VER_CODE=$(getprop magisk.version_code)
 
-export DING_CONFIG_FILE="/data/adb/http_proxy/config.toml"
+export HTTP_PROXY_CONFIG_PATH="/data/adb/http_proxy/config.toml"
 
 /data/adb/http_proxy/http_proxy &
